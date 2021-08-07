12e041ee5e7b2bb1336397b09f612b896b99e9b7
【daiki】 <daiki20010705@icloud.com>
2021-08-07 21:27:39 +0900

    引数の理解があいまい　　is-activeを使えば簡単にできる？

--- src/js/index.js
+++ src/js/index.js
@@ -10,9 +10,9 @@ function creatDocument(question_number ) {
                   +      '<h1>' + question_number + 'この地名はなんて読む?</h1>'
                   +      '<img src="../img/' + question_number +  '.png">'
                   +      '<ul>'
-                  +         '<li class="contents__list__' + question_number + '" onclick="changeColor('+ question_number + ')">' + choiceText[question_number][0] + '</li>'
-                  +         '<li class="contents__list__' + question_number + '" onclick="changeColor('+ question_number + ')">' + choiceText[question_number][1] + '</li>'
-                  +         '<li class="contents__list__' + question_number + '" onclick="changeColor('+ question_number + ')">' + choiceText[question_number][2] + '</li>'
+                  +         '<li class="contents__list" id="contents__list__' + question_number + '" onclick="changeColor('+ question_number + ')">' + choiceText[question_number][0] + '</li>'
+                  +         '<li class="contents__list" id="contents__list__' + question_number + '" onclick="changeColor('+ question_number + ')">' + choiceText[question_number][1] + '</li>'
+                  +         '<li class="contents__list" id="contents__list__' + question_number + '" onclick="changeColor('+ question_number + ')">' + choiceText[question_number][2] + '</li>'
                   +      '</ul>'
                   +   '</div>';
 
@@ -28,17 +28,18 @@ document.write(htmlDocument);
 // lists[question_number][0]
 
 // クリック時の処理
+
 function changeColor(question_number) {
-  let lists = document.getElementsByClassName('contents__list__' + question_number);
-  lists = Array.from(lists);
-  lists.forEach(list => {
-    list.style.backgroundColor = '#287dff';
-  });
+  let lists = document.getElementById('contents__list__' + question_number);
+  // lists.style.backgroundColor = '#287dff';
+
+  console.log(lists);
 
 
   
 }
 
+changeColor(0);
 
 
 
@@ -46,3 +47,23 @@ for (let i = 0; i < choiceText.length; i++) {
   creatDocument(i);
 }
 // console.log("ok");
+
+
+
+// function btnAction(btnDOM,btnId){
+//   　　// 各ボタンをイベントリスナーに登録
+//   　　btnDOM.addEventListener("click", function(){
+//   　　// activeクラスの追加と削除
+//   　　// thisは、クリックされたオブジェクト
+//   　　this.classList.toggle('active');
+  
+//   　　// クリックされていないボタンにactiveがついていたら外す
+//   　　for (var i = btn.length - 1; i >= 0; i--) {
+//   　　if(btnId !== i){
+//   　　　　if(btn[i].classList.contains('active')){
+//   　　　　　　btn[i].classList.remove('active');
+//   　　　　　　}
+//   　　　　　}
+//   　　　　}
+//   　　})
+//   }
